d3631c4501362520aa5132f0e1dceaf521d28850
Felipe Vieira <felip.3lima@hotmail.com>
2024-03-15 10:41:07 -0300

    [init] - first commit

--- src/services/gitHubApi.ts
+++ src/services/gitHubApi.ts
@@ -4,7 +4,7 @@ export const GithubApi = axios.create({
   baseURL: 'https://api.github.com',
   headers: {
     'Accept': 'application/vnd.github+json',
-    'Authorization': 'Bearer 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',
+    'Authorization': 'Bearer ',
     'X-GitHub-Api-Version': "2022-11-28"
   }
 })
